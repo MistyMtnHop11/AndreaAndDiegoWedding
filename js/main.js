598c610ed7efe8bdeefef49bd915629d8f9683af
Enzo Swayne <e.a.swayne@gmail.com>
2026-03-03 18:24:46 -0500

    added countdown

--- js/main.js
+++ js/main.js
@@ -105,3 +105,25 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
+/* ========================================================
+   COUNTDOWN TIMER FUNCTIONALITY
+========================================================= */
+
+var countDownDate = new Date("2027-02-19T13:00:00").getTime();
+
+var intervalTime = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = 
+        days + " Days " + hours + " Hours " + minutes + " Minutes " + seconds + " Seconds ";
+
+    if (distance < 0) {
+        clearInterval(intervalTime);
+        document.getElementById("countdown").innerHTML = "...And they lived happily ever after!";
+    }
+}, 1000);
